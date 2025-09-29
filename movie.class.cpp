f985f0f2ab9a83e623ea954c98f4585564215244
Phillip Wagner <m0ssyblue7@gmail.com>
2025-09-28 19:37:34 -0700

    Second addition to movie.class.cpp

--- movie.class.cpp
+++ movie.class.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
+using namespace std;
+
+class Movie {
+    private:
+
+
+    public:
+    void--
+    void--
+}
 
 
 int main () {
-
-
-
-
-
-
-
-
-
+    Movie test;
+    test.setTitle("something");
+    test.setYearReleased("sometihn");
+    test.setScreenWriter("something");
+    test.print();
 
     return 0;
 }
