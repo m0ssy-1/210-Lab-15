81e9b1773c10ba195fc0765d1d9ded79c255eb08
Phillip Wagner <m0ssyblue7@gmail.com>
2025-09-28 20:38:42 -0700

    Final addition to movie.class.cpp

--- movie.class.cpp
+++ movie.class.cpp
@@ -1,50 +1,62 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
 
 class Movie {
     private:
         string title;
-        int setYearReleased;
-        string setScreenWriter;
+        int yearReleased;
+        string screenWriter;
 
     public:
     void setTitle(const string& t) {title = t;}
-    void setYearReleased(int y) {setYearReleased = y;}
-    void setScreenWriter(const tring& sw) {setScreenWriter = sw;}
+    void setYearReleased(int y) {yearReleased = y;}
+    void setScreenWriter(const string& sw) {screenWriter = sw;}
 
     void print() const {
-        cout << "movie" << setScreenWriter << endl;
-        cout << "yearreleased" << setYearReleased << endl;
-        cout << "screenwriter" << title << endl;
+        cout << "Movie: " << screenWriter << endl;
+        cout << "Year Released: " << yearReleased << endl;
+        cout << "Screen Writer: " << title << endl;
+        cout << endl;
     }
 };
 
 
 int main () {
-    ifstream intputFile("input.txt");
+    ifstream inputFile("input.txt");
     if (!inputFile) {
-        cout << "Error: coult not open 'input.txt'" << endl;
+        cout << "Error: Could not open 'input.txt'" << endl;
         return 0;
     }
 
+    vector<Movie> movies;
+
     string title, writer;
+
     int year;
-// reads firsr three lines
-    getline(inputFile, title);
-    inputFile >> year;
-    inputFile.ignore();
-    getline(intputFile, writer);
 
-//create/print movie
-    Movie test;
+// reads until end of file
+    while (getline(inputFile, title)) {
+        inputFile >> year;
+        inputFile.ignore();
+        getline(inputFile, writer);
 
-    test.setTitle("something");
-    test.setYearReleased("sometihn");
-    test.setScreenWriter("something");
+    Movie temp;
 
-    test.print();
+    temp.setTitle(title);
+    temp.setYearReleased(year);
+    temp.setScreenWriter(writer);
+
+    movies.push_back(temp);
+    }
+
+    inputFile.close();
+
+for (int i = 0; i < movies.size(); i++){
+    movies[i].print();
+}
 
     return 0;
 }
